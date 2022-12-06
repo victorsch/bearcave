f4fc9f4b78a7b46ed178cb00807b0c8604ddb100
victor.schiavo <victors@yellowbasket.io>
2022-12-06 18:10:59 -0500

    removed old script

--- honeypot.py
+++ honeypot.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
 import os
-
-# Set the initial working directory to the root directory
-os.chdir("/")
+import json
+from datetime import datetime
 
 # Create a list of common Linux files
 files = ["bin", "dev", "etc", "home", "lib", "lib64", "media", "mnt", "opt", "proc", "root", "run", "sbin", "srv", "sys", "tmp", "usr", "var"]
 
+# Add some fake users to the list of files
+fake_users = ["alice", "bob", "chris", "dave", "eve", "frank", "grace", "hannah", "ian", "jessica"]
+
 # Print a welcome message
 print("Welcome to the fake Linux terminal honeypot")
 
+# Set the initial fake directory to the root directory
+fake_dir = "/"
+
+logs = []
+existing_log = ""
+
+# Get the current date
+now = datetime.now()
+date = now.strftime("%m-%d-%Y")
+
+log_file = f"logs/logs-{date}.json"
+if not os.path.exists(log_file):
+    with open(log_file, "w") as f:
+        # Initialize the log file with an empty list of logs
+        f.write("")
+else:
+  existing_log = json.loads(open(log_file, "r+").read())
+  for log in existing_log:
+    logs.append(log)
 while True:
   # Display the current working directory
-  cwd = os.getcwd()
-  print(f"{cwd}$ ", end="")
+  print(f"{fake_dir}$ ", end="")
 
   # Get the command from the user
   cmd = input()
@@ -28,21 +48,48 @@ while True:
 
   # If the user entered the "ls" command, print a list of common Linux files
   if tokens[0] == "ls":
-    for file in files:
-      print(file, end="  ")
+    if fake_dir == "/home":
+      # If the fake_dir is in the home directory, print the fake users
+      for user in fake_users:
+        print(user, end="  ")
+    else:
+      # Otherwise, print the common Linux files
+      for file in files:
+        print(file, end="  ")
     print()
 
-  # If the user entered the "cd" command, change the working directory
+  # If the user entered the "cd" command, pretend to change the working directory
   elif tokens[0] == "cd":
     if len(tokens) == 1:
-      # If no directory was specified, go to the home directory
-      os.chdir("/home")
+      # If no directory was specified, pretend to go to the home directory
+      fake_dir = "/home"
+      print(fake_dir)
+    elif tokens[1] == "..":
+        # If the user specified "..", pretend to move back to the parent directory
+        fake_dir = "/"
+        print(fake_dir)
     elif tokens[1] in files:
-      # If a valid directory was specified, go to that directory
-      os.chdir(f"/{tokens[1]}")
+      # If a valid directory was specified, pretend to go to that directory
+      fake_dir = f"/{tokens[1]}"
+      print(fake_dir)
     else:
       # If an invalid directory was specified, print an error message
       print("No such file or directory")
   else:
     # If the user entered an invalid command, print an error message
     print("Command not found")
+
+  # Log the user input to a json file
+  now = datetime.now()
+  date = now.strftime("%m-%d-%Y")
+  log = {
+    "date": date,
+    "input": cmd
+  }
+  logs.append(log)
+  # with open(f"logs-{date}.json", "a") as f:
+  #   json.dump(log, f)
+
+f = open(log_file, "w")
+json.dump(logs, f)
+f.close()
